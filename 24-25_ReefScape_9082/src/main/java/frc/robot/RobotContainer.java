eb4ba172ad6a3f6a5fe9c8fd02dccfa4fca52922
Brandon Seamer <brandonseamer@gmail.com>
2025-01-29 20:55:21 -0600

    feat: add lift driving via joystick 2

--- 24-25_ReefScape_9082/src/main/java/frc/robot/RobotContainer.java
+++ 24-25_ReefScape_9082/src/main/java/frc/robot/RobotContainer.java
@@ -18,6 +18,8 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
+import frc.robot.subsystems.ElementLift;
+
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
 
@@ -42,6 +44,8 @@ public class RobotContainer {
 
     public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
 
+    public final ElementLift elementLift = new ElementLift();
+
     // Path Follower
     private final SendableChooser<Command> autoChooser;
 
@@ -49,12 +53,13 @@ public class RobotContainer {
     public RobotContainer() {
         autoChooser = AutoBuilder.buildAutoChooser("Britney Auto");
         SmartDashboard.putData("Auto Mode", autoChooser);
+        drivetrain.registerTelemetry(logger::telemeterize);
         configureBindings();
     }
 
     private void configureBindings() {
-        // Note that X is defined as forward according to WPILib convention,
-        // and Y is defined as to the left according to WPILib convention.
+        
+        //Subsystem Default Commands        
         drivetrain.setDefaultCommand(
             // Drivetrain will execute this command periodically
             drivetrain.applyRequest(() ->
@@ -64,11 +69,15 @@ public class RobotContainer {
             )
         );
         
-        joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
-        joystick.b().whileTrue(drivetrain.applyRequest(() ->
-            point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))
-        ));
+        elementLift.setDefaultCommand(new RunCommand(()->{
+            elementLift.lift(-joystick2.getY());
+        }));
 
+
+        //Joystick 1 button bindings:
+        joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
+        joystick.b().whileTrue(drivetrain.applyRequest(() -> point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))));
+        
         // Run SysId routines when holding back/start and X/Y.
         // Note that each routine should be run exactly once in a single log.
         //joystick.back().and(joystick.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
@@ -76,10 +85,11 @@ public class RobotContainer {
         //joystick.start().and(joystick.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
         //joystick.start().and(joystick.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
 
-        // reset the field-centric heading on left bumper press
+        // Reset the field-centric heading on left bumper press
         joystick.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
 
-        drivetrain.registerTelemetry(logger::telemeterize);
+
+        
         
     }
 
