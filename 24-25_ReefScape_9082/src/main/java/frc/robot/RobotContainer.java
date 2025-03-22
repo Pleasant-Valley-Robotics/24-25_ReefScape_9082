28a3e874e797382f314a42827b9f23fc6b0cae71
Brandon Seamer <brandonseamer@gmail.com>
2025-03-21 22:58:27 -0500

    Option1 cleanup

--- 24-25_ReefScape_9082/src/main/java/frc/robot/RobotContainer.java
+++ 24-25_ReefScape_9082/src/main/java/frc/robot/RobotContainer.java
@@ -15,7 +15,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants.elementLiftConstants;
@@ -31,7 +30,6 @@ import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
 
 public class RobotContainer {
-    private double drivingScalarConstant = 1;
     private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
     private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
 
